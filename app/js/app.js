a570915815051239801876cabce3d9dfd2b32667
Klaas Cuvelier <klaas@showpad.com>
2013-05-12 10:53:22 +0200

    change template + controller + use app constant for url

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,9 @@ var BlosoApp = angular.module('BlosoApp', [])
     }])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/select', { templateUrl: 'partials/route-list.html', controller: 'SelectController' })
+            .when('/select', { templateUrl: 'partials/select.html', controller: 'SelectController' })
             // .when('/route/:cityId/:routeId', { templateUrl: 'partials/route.html', controller: 'RouteController' })
             .otherwise({redirectTo: '/select'});
-    }]);
+    }])
+
+    .constant('url-cities', '/data/gemeentes.json');
