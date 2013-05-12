8bad099f3314fc525528e18b2a61a1efe85ae8cb
Klaas Cuvelier <klaas@showpad.com>
2013-05-12 10:52:58 +0200

    update bloso service

--- app/js/services/bloso.service.js
+++ app/js/services/bloso.service.js
@@ -2,7 +2,7 @@
 // Bloso service, providing data to the app
 // Methods in this service should always return promises
 //
-BlosoApp.factory('$bloso', ['$http', '$q', function $blosoService($http, $q) {
+BlosoApp.factory('$bloso', ['$http', '$q', '$cache', 'url-cities', function $blosoService($http, $q, $cache, urlCities) {
 
     var $bloso = {
         cities  : [],
@@ -15,15 +15,38 @@ BlosoApp.factory('$bloso', ['$http', '$q', function $blosoService($http, $q) {
          */
         getCities: function () {
 
-            var result = $q.defer;
+            var deferred    = $q.defer(),
+                _this       = this;
 
+            // if cities are not loaded yet, check if they are in cache
+            // if not, load from server
             if (!this.cities.length) {
 
+                $cache.get('cities').then(
+                    function (data) {
+                        _this.cities = data;
+                        deferred.resolve(data);
+                    },
+                    function () {
+                        $http.get(urlCities)
+                            .success(function (response) {
+                                _this.cities = response;
+                                // don't need the deferred here, if it fails to store in cache, no biggy
+                                $cache.set('cities', response);
+                                deferred.resolve(response);
+                            })
+                            .error(function () {
+                                // well this is embarassing, apparently data doesn't load
+                                deferred.reject();
+                            });
+                    }
+                );
+
             } else {
-                $promise.resolve(this.cities);
+                deferred.resolve(this.cities);
             }
 
-            return result.promise;
+            return deferred.promise;
         }
 
 
