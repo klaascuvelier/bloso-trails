9fc78e07bda460e48582f86855d84b02ad3ff735
Klaas Cuvelier <klaas@showpad.com>
2013-05-21 15:18:34 +0200

    add failing test

--- test/unit/services/bloso.service.js
+++ test/unit/services/bloso.service.js
@@ -8,6 +8,10 @@ describe('service', function () {
             expect($bloso).not.toEqual(null);
         }));
 
+        it('should fail', inject(function ($bloso) {
+            expect(true).toEqual(false);
+        }));
+
 /*
         it('should have classes as items', inject(function($list) {
 
